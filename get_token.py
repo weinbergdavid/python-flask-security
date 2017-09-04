01347623e579fc2dad8f43c1f9ff4542de9b84c0
David Weinberg <davidweinberg0@gmail.com>
2017-09-04 14:08:10 +0300

    Update get_token.py

--- get_token.py
+++ get_token.py
@@ -1,7 +1,7 @@
 import json
 import requests
 
-r = requests.post('http://127.0.0.1:5001/login', data=json.dumps({'email':'test@example.com', 'password':'test123'}), headers={'content-type': 'application/json'})
+r = requests.post('http://127.0.0.1:5001/login', json={'email':'test@example.com', 'password':'test123'})
 
 token = r.json()['response']['user']['authentication_token']
 print(token)
